21c6efe475a20e3e5cb398cb1ff5b0949ea7b607
YousefMafi96 <157359402+YousefMafi96@users.noreply.github.com>
2026-02-25 13:13:44 +0200

    Adjust plot size and layout for bar chart
    Updated plot dimensions and layout settings.

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ with col2:
 
     # ---- Fixed size ----
 fig.update_layout(
-    width=800,
-    height=600,
+    width=600,
+    height=400,
     barmode='group',
 
     plot_bgcolor='white',
@@ -81,6 +81,7 @@ fig.update_layout(
 )
 
 st.plotly_chart(fig, use_container_width=False)
+
 
 
 
